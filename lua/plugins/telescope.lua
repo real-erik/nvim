bb1372f5b1c0854794fa7f66d96b3b8246c8f428
Erik <ewaasen@ucdavis.edu>
2023-03-22 10:14:26 -0700

    telescope delete buffers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,18 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.1",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			pickers = {
+				buffers = {
+					mappings = {
+						n = {
+							["d"] = "delete_buffer",
+						},
+					},
+				},
+			},
+		})
+	end,
 }
