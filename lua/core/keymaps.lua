a02826670e49076250bc57d77f4b4ed3b92d3d51
Erik <ewaasen@ucdavis.edu>
2023-03-23 11:32:08 -0700

    add git keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,14 +10,19 @@ local telescope = require("telescope.builtin")
 -- telescope keymaps
 keymap.set("n", "<leader><space>", telescope.buffers, { desc = "[ ] Find existing buffers" })
 keymap.set("n", "<leader>?", telescope.oldfiles, { desc = "[?] Find recently opened files" })
-keymap.set("n", "<leader>f", telescope.find_files, { desc = "[F]iles" })
-keymap.set("n", "<leader>g", telescope.live_grep, { desc = "[G]rep" })
+keymap.set("n", "<leader>sf", telescope.find_files, { desc = "[F]iles" })
+keymap.set("n", "<leader>sg", telescope.live_grep, { desc = "[G]rep" })
 
 -- diffview keymaps
 keymap.set("n", "<leader>do", ":DiffviewOpen<CR>")
 keymap.set("n", "<leader>dc", ":DiffviewClose<CR>")
 keymap.set("n", "<leader>dh", ":DiffviewFileHistory<CR>")
 keymap.set("n", "<leader>dr", ":DiffviewRefresh<CR>")
+
+-- fugitive keymaps
+keymap.set("n", "<leader>gs", ":Git status<CR>")
+keymap.set("n", "<leader>gb", ":Git branch<CR>")
+keymap.set("n", "<leader>gc", ":Git commit<CR>")
 
 -- window control
 keymap.set("n", "<leader>w", "<C-w>")
