5c036695656e823e231377c9afe1ee666082570d
Erik <ewaasen@ucdavis.edu>
2023-09-22 16:18:03 -0700

    use standard go to definition

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -31,7 +31,7 @@ return {
 
       nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
       nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-      nmap("gd", fzf.lsp_definitions, "[G]oto [D]efinition")
+      nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
       nmap("gr", fzf.lsp_references, "[G]oto [R]eferences")
       nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
       nmap("fd", vim.diagnostic.open_float, "[F]loat [D]escription")
