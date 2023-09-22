4bf0ae97f5864f9277ca360ad1ef89d9325e2ea8
Erik <ewaasen@ucdavis.edu>
2023-09-22 16:18:47 -0700

    add marksman

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -72,6 +72,11 @@ return {
       capabilities = capabilities,
     })
 
+    lspconfig.marksman.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+
     lspconfig.lua_ls.setup({
       on_attach = on_attach,
       capabilities = capabilities,
