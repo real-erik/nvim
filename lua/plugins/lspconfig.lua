57dac0e9a7ca6c398ce591f2cc41193a67b792f5
Erik <ewaasen@ucdavis.edu>
2023-09-22 16:19:59 -0700

    add neodev for nvim/lua development

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,6 +6,9 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   config = function()
+    -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
+    require("neodev").setup({})
+
     -- import lspconfig plugin safely
     local lspconfig_status, lspconfig = pcall(require, "lspconfig")
     if not lspconfig_status then
